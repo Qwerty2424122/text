d8fbbfc066036f11a48d804af21d2836f287e9a0
Bharat Bhushan Sharma <34498660+Qwerty2424122@users.noreply.github.com>
2018-12-23 20:38:11 -0800

    Delete vue_templ.js

--- vue_templ.js
+++
@@ -1,9 +0,0 @@
-var vm = new Vue({
-    el: '#vue_det',
-    data: {
-       firstname : "sharma",
-       lastname  : "bharat",
-       htmlcontent : "<div><h1>Vue Js Template</h1></div>",
-       imgsrc : "./image/img.png"
-    }
- })
