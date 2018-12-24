ca13f84c2118a1b131ec0e9a9dca1dfce5faffe0
Bharat Bhushan Sharma <34498660+Qwerty2424122@users.noreply.github.com>
2018-12-23 20:58:14 -0800

    yes
    fina commit

--- js/vue_templ.js
+++ js/vue_templ.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var vm = new Vue({
     el: '#vue_det',
     data: {
@@ -6,14 +5,6 @@ var vm = new Vue({
        lastname  : "bharat",
        htmlcontent : "<div><h1>Vue Js Template</h1></div>",
        imgsrc : "./image/img.png"
-    }
-=======
-var vm = new Vue({
-    el: '#vue_det',
-    data: {
-       firstname : "sharma",
-       lastname  : "bharat",
-       htmlcontent : "<div><h1>Vue Js Template</h1></div>"
-    }
->>>>>>> 0a53a77c71062e40fd479c9017081aa695743373
- })
+    },
+});
+
