08e05d6040e5e2f4c6b543c6f833056a72d2b7d6
Bharat Bhushan Sharma <34498660+Qwerty2424122@users.noreply.github.com>
2018-12-23 20:41:00 -0800

    Commit and comfirm changes locally
    yes

---
+++ js/vue_templ.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+    el: '#vue_det',
+    data: {
+       firstname : "sharma",
+       lastname  : "bharat",
+       htmlcontent : "<div><h1>Vue Js Template</h1></div>",
+       imgsrc : "./image/img.png"
+    }
+ })
